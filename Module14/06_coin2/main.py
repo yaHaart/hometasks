8b2c527657356e62f5ab599ab8647b73b5f3d8c2
haart <haart@yandex.ru>
2021-01-17 18:56:44 +0300

    task 6

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+
+print('Введите координаты монетки:')
+x = float(input('X: '))
+y = float(input('Y: '))
+radius = float(input('Введите радиус: '))
+
+if x * y <= radius:
+    print('Монетка где-то рядом')
+else:
+    print('Монетки в области нет')
