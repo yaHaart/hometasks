98f22454799a54a0699f5cbedffb08c5c62ee83f
haart <haart@yandex.ru>
2021-01-17 19:05:26 +0300

    task 7

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+
+start = int(input('Введите начало промежутка ' ))
+end = int(input('Введите конец промежутка ' ))
+
+for i in range(start, end + 1):
+    line = str(i)
+    if line[1] == line[2] == line[3] or line[0] == line[2] == line[3] or line[0] == line[1] == line[3] or line[0] == line[1] == line[2]:
+        print(i)
