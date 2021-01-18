45730608987bc1cda0f4e1f21859d4c3adb3c5c4
haart <haart@yandex.ru>
2021-01-18 21:59:21 +0300

    commit lesson 14

---
+++ Module14/03_sum_and_dif/main.py
@@ -0,0 +1,21 @@
+def summ_digit(n):
+    line = str(n)
+    summ = 0
+    for char in line:
+        summ += int(char)
+    return summ
+
+
+def length(n):
+    length = len(str(n))
+    return length
+
+
+n = int(input('Введите число: '))
+summ = summ_digit(n)
+length = length(n)
+print('Сумма чисел:', summ)
+print('Кол-во цифр в числе: ', length)
+print('Разность суммы и кол-ва цифр:', summ - length)
+
+# зачёт! 🚀
---
+++ Module14/04_reverse_num/main.py
@@ -0,0 +1,17 @@
+def revers_number(number):
+    line = str(number)
+    a = line.split('.')
+    new_line = a[0][::-1] + '.' + a[1][::-1]
+    return float(new_line)
+
+
+number1 = float(input('Введите первое число: '))
+number2 = float(input('Введите второе число: '))
+
+rev_num1 = revers_number(number1)
+rev_num2 = revers_number(number2)
+print('Первое число наоборот: ', rev_num1)
+print('Второе число наоборот: ', rev_num2)
+print('Сумма: ', rev_num1 + rev_num2)
+
+# зачёт! 🚀
