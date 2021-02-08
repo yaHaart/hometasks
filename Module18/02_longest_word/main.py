77fd4d61730cc2c058f5d573580b429c10d6cd6a
haart <haart@yandex.ru>
2021-02-08 18:44:39 +0300

    task2 commit

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+words_list = string.split()
+max_word = ''
+count = 0
+for word in words_list:
+    if len(word) > count:
+        max_word = word
+        count = len(word)
+
+print(f'Самое длинное слово "{max_word}" содержит {count} символов')
