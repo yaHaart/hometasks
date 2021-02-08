42fd6cd85dbcc0a2de68c15998f55b5fa327c6bb
haart <haart@yandex.ru>
2021-02-08 19:10:18 +0300

    task3 commit

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+bad_simbols = '@№\$%^&*()'
+string = input('Введите название файла: ')
+
+if not string.endswith('.txt') and not string.endswith('.doc'):
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+elif string[0] in bad_simbols:
+    print('Ошибка: название начинается на один из специальных символов')
+else:
+    print('Файл назван верно.')
