b59d494937acbd00186ffe7968987705ac813689
haart <haart@yandex.ru>
2021-02-09 20:10:05 +0300

    task5 commit

--- Module18/05_captain_Flint/main.py
+++ Module18/05_captain_Flint/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import re
+
+string = 'South 17, North 13, East 15, West 11'
+opt = re.sub(r'[^\w\s]', '', string)
+str_list = opt.split()
+coordinates = {str_list[x]: int(str_list[x + 1]) for x in range(0, len(str_list), 2)}
+print(coordinates)
+oy = coordinates['North'] - coordinates['South']
+ox = coordinates['East'] - coordinates['West']
+print('Координаты: ', ox, oy)
