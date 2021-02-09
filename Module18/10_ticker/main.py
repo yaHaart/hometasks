c3898d968ac8ccc25eef0a846695b900a50a337e
haart <haart@yandex.ru>
2021-02-09 20:50:41 +0300

    task10 commit

--- Module18/10_ticker/main.py
+++ Module18/10_ticker/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+line1 = 'asdfgh'
+line2 = 'sdfgha'
+flag = False
+new_line = ''
+sdvig = 0
+# if len(line2) != len(line1):
+#     flag = False # выход
+
+for i in range(1, len(line1)):
+    for j in range(len(line1)):
+        new_line += line1[(j + i) % len(line1)]
+    if new_line == line2:
+        flag = True
+        sdvig = i
+        break
+    if flag:
+        break
+    new_line = ''
+if flag:
+    print('Первая строка получается из второй со сдвигом', sdvig)
+else:
+    print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
