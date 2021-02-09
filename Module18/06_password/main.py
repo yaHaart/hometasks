98dbd1e2c705da9264cfd74b6787bb66f8f921ca
haart <haart@yandex.ru>
2021-02-09 17:09:51 +0300

    task6 commit

--- Module18/06_password/main.py
+++ Module18/06_password/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+upper_chars = 0
+numbers = 0
+flag = True
+while flag:
+    string = input('введите пароль: ')
+    for char in string:
+        if char.isupper():
+            upper_chars += 1
+        if char.isdigit():
+            numbers += 1
+    if upper_chars < 1 or numbers < 3 or len(string) < 8:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+    else:
+        print('Это надёжный пароль!')
+        flag = False
