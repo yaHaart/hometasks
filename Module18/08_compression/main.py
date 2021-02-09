46e7f40e276b05ffd8599bd675111590bb75c712
haart <haart@yandex.ru>
2021-02-09 18:04:49 +0300

    task8 commit

--- Module18/08_compression/main.py
+++ Module18/08_compression/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+string = input('Введите строку ')
+zip_str = ''
+count = 0
+for i in range(1, len(string)):
+    count += 1
+    if string[i - 1] != string[i]:
+        zip_str += (string[i - 1] + str(count))
+        count = 0
+    elif i == len(string) - 1:
+        zip_str += (string[i - 1] + str(count + 1))
+
+
+print('закодированная строка ', zip_str)
+
