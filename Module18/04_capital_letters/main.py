9be8086c03395037b30c987a8f1e4353fad7f7dc
haart <haart@yandex.ru>
2021-02-09 16:06:25 +0300

    task4 commit

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+word_list = string.split()
+new_string = ''
+for word in word_list:
+    if word[0].islower():
+        word = word.capitalize()
+    new_string += word + ' '
+print(new_string)
