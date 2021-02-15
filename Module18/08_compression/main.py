0290d690d5b493c4ef4bb67a409fc55a9158e3f0
haart <haart@yandex.ru>
2021-02-15 23:18:04 +0300

    task08 bugfix

--- Module18/08_compression/main.py
+++ Module18/08_compression/main.py
@@ -1,19 +1,15 @@
 string = input('Введите строку ')
 zip_str = ''
 count = 0
-for i in range(1, len(string)):  # TODO гораздо проще задать интервал range(len(string))
+for i in range(len(string)):
     count += 1
-    if string[i - 1] != string[i]:
-        zip_str += (string[i - 1] + str(count))
+    print(i)
+    if i == len(string) - 1:
+        zip_str += (string[i] + str(count))
+        print('последний символ')
+    elif string[i] != string[i + 1]:
+        zip_str += (string[i] + str(count))
+        print('непоследний')
         count = 0
-    elif i == len(string) - 1:
-        zip_str += (string[i - 1] + str(count + 1))
-
 
 print('закодированная строка ', zip_str)
-# TODO последний символ не попал в закодированный ответ:
-#  Введите строку aaAAbbсaaaA
-#  закодированная строка  a2A2b2с1a3
-#  .
-#  Должно получиться:
-#  Закодированная строка: a2A2b2a3A1
