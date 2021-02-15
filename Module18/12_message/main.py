362e5c7b6de50ac61cf6d40def8ef12e2ad409d2
haart <haart@yandex.ru>
2021-02-15 23:02:23 +0300

    task12 bugfix

--- Module18/12_message/main.py
+++ Module18/12_message/main.py
@@ -2,13 +2,13 @@ line = input(' Введите строку ')
 splited_line = line.split()
 punctuation_line = '!.,?:;'
 
-punkt_char = ' '
+punktuation_char = ' '
 for word in splited_line:
     if word[-1] in punctuation_line:
-        punkt_char = word[-1] + ' '
+        punktuation_char = word[-1] + ' '
         word = word[:len(word) - 1]
-    new_line = ''.join(reversed(word)) + punkt_char
+    new_line = ''.join(reversed(word)) + punktuation_char
     print(new_line, end='')
-    punkt_char = ' '
+    punktuation_char = ' '
 
-# TODO нейминг! Исправить названия переменных, записанных транслитом (перевести на английский)
+
