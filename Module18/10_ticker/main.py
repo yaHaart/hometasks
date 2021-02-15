f9a1025dd990bcbd5f9a16780cc05e5d94947870
haart <haart@yandex.ru>
2021-02-15 22:25:38 +0300

    task10 bugfix

--- Module18/10_ticker/main.py
+++ Module18/10_ticker/main.py
@@ -2,19 +2,19 @@ line1 = 'asdfgh'
 line2 = 'sdfgha'
 flag = False
 new_line = ''
-sdvig = 0  # TODO нейминг! Транслит в названиях переменных недопускается. Здесь более подходящее название - shift
+shift = 0
 
 for i in range(1, len(line1)):
     for j in range(len(line1)):
         new_line += line1[(j + i) % len(line1)]
     if new_line == line2:
         flag = True
-        sdvig = i
+        shift = i
         break
     if flag:
         break
     new_line = ''
 if flag:
-    print('Первая строка получается из второй со сдвигом', sdvig)
+    print('Первая строка получается из второй со сдвигом', shift)
 else:
     print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
