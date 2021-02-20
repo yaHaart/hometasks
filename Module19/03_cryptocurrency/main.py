988c7db6d874ff7700c442af686dfc2d79253998
haart <haart@yandex.ru>
2021-02-20 17:22:52 +0300

    task03 done

--- Module19/03_cryptocurrency/main.py
+++ Module19/03_cryptocurrency/main.py
@@ -44,5 +44,26 @@ data = {
     ]
 }
 
+# Вывести списки ключей и значений словаря
+for key, value in data.items():
+    print('Ключи и значения словаря ', key, value)
+print()
 
-# TODO здесь писать код
+# В “ETH” добавить ключ “total_diff” со значением 100
+data['ETH']['total_diff'] = 100
+print(data['ETH'])
+print()
+
+# Внутри “fst_token_info” значение ключа “name” поменять с “fdf” на “doge”
+data['tokens'][0]['fst_token_info']['name'] = 'doge'
+print(data['tokens'][0]['fst_token_info'])
+print()
+
+# Удалить “total_out” из tokens и присвоить его значение в “total_out” внутри “ETH”
+data['ETH']['totalOut'] = data['tokens'][1].pop('total_out')
+print(data['ETH']['totalOut'])
+print()
+
+# Внутри "sec_token_info" изменить название ключа “price” на “total_price”.
+(data['tokens'][0]['fst_token_info']['total_price']) = data['tokens'][0]['fst_token_info'].pop('price')
+print(data['tokens'][0]['fst_token_info'])
