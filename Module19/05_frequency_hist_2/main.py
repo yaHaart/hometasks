43f6e12cbf07f9864580db99d434d14664c867f2
haart <haart@yandex.ru>
2021-02-21 13:45:22 +0300

    task05 done

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+line = 'Тут туту поезд'
+init_dict = dict()
+revers_dict = dict()
+temp = []
+
+for char in line:
+    if char in init_dict:
+        init_dict[char] += 1
+    else:
+        init_dict[char] = 1
+
+print(init_dict)
+
+for key, value in init_dict.items():
+    if value in revers_dict:
+        revers_dict[value].append(key)
+    else:
+        revers_dict[value] = list(key)
+
+print(revers_dict)
