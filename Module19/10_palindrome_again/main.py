ffc8b8c9cc281d16b06f21297a711604ac9f26c1
haart <haart@yandex.ru>
2021-03-03 17:18:17 +0300

    task10 done

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,3 +1,31 @@
-line = 'тут тут у'
+line = input('Введите строку: ')
 line_dict = dict()
 for char in line:
+    if char != ' ':
+        if char in line_dict:
+            line_dict[char] += 1
+        else:
+            line_dict[char] = 1
+summ = 0
+for key in line_dict:
+    summ += line_dict[key]
+
+poli = True
+count = 0
+if summ % 2 == 0:
+    # print(summ, 'чет')
+    for key in line_dict:
+        if line_dict[key] % 2 != 0:
+            poli = False
+else:
+    # print(summ, 'нечет')
+    for key in line_dict:
+        if line_dict[key] % 2 != 0:
+            count += 1
+    if count != 1:
+        poli = False
+
+if poli:
+    print('Можно сделать палиндромом')
+else:
+    print('Нельзя сделать палиндромом')
