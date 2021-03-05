009dafe0e23f348984c9f2f93568ad8347d79a99
haart <haart@yandex.ru>
2021-03-05 16:58:10 +0300

    task04 done

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+
+my_tuple = ('q', 'w', 'e', 'r', 't', 'a', 'c', 'e', 'c', 's')
+char_to_count = 'e'
+my_list = list(my_tuple)
+# [f(x) if condition else g(x) for x in sequence]
+new_list = [char if my_list.index(char_to_count) <= my_list.index(char) else
+# new_list = [char for char in my_list
+#             if my_list.index(char_to_count) <= my_list.index(char) <
+#             my_list[my_list.index(char_to_count) + 1:].index(char_to_count) + 1 + my_list.index(char_to_count)]
+print(new_list)
--- Module20/04_players/main.py
+++ Module20/04_players/main.py
@@ -3,5 +3,13 @@ players = {
     ("Bob", "Robbin"): (7, 5, 14),
     ("Rob", "Bobbin"): (12, 8, 2)
 }
-
-# TODO здесь писать код
+new_list = list()
+temp_list = list()
+for player in players:
+    temp_list.append(player[0])
+    temp_list.append(player[1])
+    for i in range(3):
+        temp_list.append(players[player][i])
+    new_list.append(tuple(temp_list))
+    temp_list.clear()
+print(new_list)
