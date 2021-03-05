81c5efad4e8360ad7a6b453a28d990dbebb14334
haart <haart@yandex.ru>
2021-03-05 18:09:46 +0300

    task08 done

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+db = {
+    ('Сидоров', 'Никита'): '+79873434125',
+    ('сидорова', 'Алина'): '+79873434154',
+    ('Сидоров', 'Павел'): '+79873436935',
+    ('Иванов', 'Иван'): '+79836814125'
+}
+
+
+def surname_find(surname):
+    for human in db:
+        if human[0].lower().find(surname.lower()) >= 0:
+            print(human[0], human[1], db[human])
+    print()
+
+
+def new_human(surname, name, telephone):
+    tuple_index = tuple([surname, name])
+    if tuple_index in db:
+        print('Обновил номер телефона для', tuple_index)
+        db[tuple_index] = telephone
+    else:
+        print('Контакт добавлен')
+        db[tuple_index] = telephone
+        for human in db:
+            print(human, db[human])
+    print()
+
+
+while True:
+    print('Выход - 0')
+    print('Поиск по фамилии - 1')
+    action = int(input('Добавить контакт - 2 '))
+    print()
+    if action == 1:
+        surname = input('Введите фамилию ')
+        surname_find(surname)
+    elif action == 2:
+        surname = input('Введите фамилию ')
+        name = input('Введите имя ')
+        telephone = input('Введите номер телефона ')
+        new_human(surname, name, telephone)
+    else:
+        break
