5a6ad2bfd71499a7f189dd42c9c4bf24bede8f56
haart <haart@yandex.ru>
2021-03-05 17:46:12 +0300

    task07 done

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+origin_tuple = (2, 5, 7, 3, 8, 2, 4.4, 4, 6, 9, 0, 4, 1, 5)
+
+
+def tuple_sort(some_tuple):
+    list_sorted = list(some_tuple)
+    list_sorted.sort()
+    count = 0
+    for number in list_sorted:
+        if type(number) is not int:
+            count += 1
+
+    if count == 0:
+        sorted_tuple = tuple(list_sorted)
+        return sorted_tuple
+    else:
+        return origin_tuple
+
+
+print(origin_tuple)
+print(tuple_sort(origin_tuple))
