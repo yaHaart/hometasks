e83164fef546ee67ba3843ae2a7d9b5d047b8532
haart <haart@yandex.ru>
2021-03-09 16:17:26 +0300

    task10 done

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+line = 'abcd'
+# some_iterable_object = [10, 20, 30, 40, 50]
+some_iterable_object = (10, 20, 30, 40, 50)
+# some_iterable_object = {1: 11, 2: 22, 3: 33, 4: 44, 5: 55}
+
+# new_tuple = zip(line, tuple)
+# print(new_tuple)
+# for item in new_tuple:
+#     print(item)
+
+new_list = list()
+for i in range(min(len(line), len(some_iterable_object))):
+    new_list.append(tuple([line[i], some_iterable_object[i]]))
+print(id(new_list))
+print(new_list)
+
+
+
+
+
+
