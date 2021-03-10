78becc6cc60adb33e84b11e709e9d053245fda57
haart <haart@yandex.ru>
2021-03-10 13:13:49 +0300

    task03 done

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,10 +1,21 @@
+my_tuple = ('a', 'c', 'v', 'c', 'd', 'v', 's', 'v')
+char_to_count = 'v'
+new_tuple = tuple()
 
-my_tuple = ('q', 'w', 'e', 'r', 't', 'a', 'c', 'e', 'c', 's')
-char_to_count = 'e'
-my_list = list(my_tuple)
-# [f(x) if condition else g(x) for x in sequence]
-new_list = [char if my_list.index(char_to_count) <= my_list.index(char) else
-# new_list = [char for char in my_list
-#             if my_list.index(char_to_count) <= my_list.index(char) <
-#             my_list[my_list.index(char_to_count) + 1:].index(char_to_count) + 1 + my_list.index(char_to_count)]
-print(new_list)
+
+def new_tuple_creating(number_chars):
+    if number_chars == 0:
+        new_tuple: tuple = ()
+        return new_tuple
+    elif number_chars == 1:
+        new_tuple = my_tuple[my_tuple.index(char_to_count):]
+        return new_tuple
+    else:
+        start = my_tuple.index(char_to_count)
+        end = my_tuple.index(char_to_count, my_tuple.index(char_to_count) + 1)
+        print(start, end)
+        new_tuple = my_tuple[start:end + 1]
+        return new_tuple
+
+
+print('ответ', new_tuple_creating(my_tuple.count(char_to_count)))
