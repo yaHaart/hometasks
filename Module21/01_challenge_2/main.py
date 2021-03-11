f599a55f4b153adc9e985a45c740686fcfc56ac3
haart <haart@yandex.ru>
2021-03-11 14:38:47 +0300

    task01 done

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+max_number = 10
+
+
+def print_next_number(number):
+    if number <= max_number:
+        print(number)
+        print_next_number(number + 1)
+
+
+print_next_number(1)
