5a93b5d92fc967f7d3f22a48b669a3944ff28cbf
haart <haart@yandex.ru>
2021-03-11 15:24:41 +0300

    task03 done

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+index_number = 17
+
+
+def fibonachi(number, next_number, count):
+
+    if count < index_number:
+        count += 1
+        fibonachi(next_number, next_number + number, count)
+    else:
+        print(index_number, '-ый член ряда Фибоначчи', number)
+
+
+fibonachi(1, 1, 1)
