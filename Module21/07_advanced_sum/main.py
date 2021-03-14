c31ec9258cbc33d7503250993a8f4e39548bedd8
haart <haart@yandex.ru>
2021-03-14 13:47:46 +0300

    task07 done

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+some_list = [1, 3, 6, 8, 3, [1, 3, [1, 2, 2], 5, 5]]
+summa = 0
+
+
+def summ(structure):
+    summa_in_func = 0
+    for element_in_func in structure:
+        if isinstance(element_in_func, list):
+            summa_in_func += summ(element_in_func)
+        else:
+            summa_in_func += element_in_func
+    return summa_in_func
+
+
+for element in some_list:
+    if isinstance(element, list):
+        summa += summ(element)
+    else:
+        summa += element
+
+print(summa)
