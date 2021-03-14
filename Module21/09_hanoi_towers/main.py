bcd5734b060abde6596ab737113d49543aba1fe9
haart <haart@yandex.ru>
2021-03-14 16:18:25 +0300

    task09 done

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+
+def move(n, start, finish):
+    if n == 1:
+        print(f'переносим 1 диск со стержня № {start} на стержень № {finish}')
+    else:
+        tmp = 6 - start - finish
+        move(n - 1, start, tmp)
+        print(f'переносим {n} диск со стержня № {start} на стержень № {finish}')
+        move(n - 1, tmp, finish)
+
+
+number_of_disks = int(input('КОличество дисков: '))
+
+move(number_of_disks, 1, 3)
