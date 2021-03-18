ab1c5e743d89b19b303d17a02a5aba4be8663da4
haart <haart@yandex.ru>
2021-03-18 18:11:48 +0300

    task07 refactored

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,5 +1,4 @@
 some_list = [1, 3, 6, 8, 3, [1, 3, [1, 2, 2], 5, 5]]
-summa = 0
 
 
 def summ(structure):
@@ -11,11 +10,7 @@ def summ(structure):
             summa_in_func += element_in_func
     return summa_in_func
 
-# TODO Зачем цикл?
-for element in some_list:
-    if isinstance(element, list):
-        summa += summ(element)
-    else:
-        summa += element
+
+summa = summ(some_list)
 
 print(summa)
