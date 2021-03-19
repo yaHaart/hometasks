68cf8d48c4e19021d5a29bb00461ebb11aeba432
haart <haart@yandex.ru>
2021-03-19 17:30:04 +0300

    task01 done

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+with open('numbers.txt', 'r', encoding='utf-8') as file:
+    summ = 0
+    for line in file:
+        summ += int(line.replace(' ', ''))
+with open('answers.txt', 'w', encoding='utf-8') as file_to_write:
+    file_to_write.write(str(summ))
