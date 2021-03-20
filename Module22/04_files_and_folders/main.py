6a31e70fe3b4af490ed2de490d1b4a41f5635a7c
haart <haart@yandex.ru>
2021-03-20 14:17:38 +0300

    task04 done

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+import os
+
+
+def go_in_folder(path_to_go, file_count, file_size):
+    for i_elem in os.listdir(path_to_go):
+        if os.path.isfile(os.path.join(path_to_go, i_elem)):
+            file_count += 1
+            file_size += os.path.getsize(os.path.join(path_to_go, i_elem))
+
+        elif os.path.isdir(os.path.join(path_to_go, i_elem)):
+            file_count, file_size = go_in_folder(os.path.join(path_to_go, i_elem), file_count, file_size)
+    return file_count, file_size
+
+
+path_to_folder = '/Users/haart/PycharmProjects/python_basic/'
+summ, size = go_in_folder(path_to_folder, 0, 0)
+
+print('Количество файлов:', summ, '       Размер, байт:', size)
