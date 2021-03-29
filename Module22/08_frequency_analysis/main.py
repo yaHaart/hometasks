0bb4a9fa510e384ce8047e406a41336d0f220d73
haart <haart@yandex.ru>
2021-03-29 20:31:17 +0300

    task08 done

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+letters_dict = {}
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+with open('text.txt', 'r', encoding='UTF-8') as file:
+    text = file.read()
+
+for char in text:
+    if char.lower() in letters_dict:
+        letters_dict[char.lower()] += 1
+    else:
+        letters_dict[char.lower()] = 1
+
+count = 0
+summ = 0
+
+for key, value in letters_dict.items():
+    if key in alphabet:
+        count += 1
+        summ += value
+
+share_letters ={}
+for key, value in letters_dict.items():
+    if key in alphabet:
+        share_letters[key] = round(value / summ, 3)
+
+sorted_values = sorted(share_letters.values(), key=None, reverse=True)
+
+
+with open('analysis.txt', 'w', encoding='utf-8') as file:
+    pass
+
+with open('analysis.txt', 'a', encoding='utf-8') as file:
+    for sorted_value in sorted_values:
+        for key in share_letters.keys():
+            if sorted_value == share_letters[key]:
+                file.write(str(key) + '   ' + str(share_letters[key]) + '\n')
+                del share_letters[key]
+                break
