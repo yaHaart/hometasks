4b1ff467a7bc5c87f99c0b9525b9c0f4837c371b
haart <haart@yandex.ru>
2021-03-29 23:43:00 +0300

    task05 done

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import os.path
+
+text = 'тут какой-то текст для записи'
+while True:
+    path_input = input('введите путь и имя файла через пробел ').split()
+    new_path = '/'
+    for part in path_input:
+        if part[-4:] != '.txt':
+            new_path = os.path.join(new_path, part)
+        else:
+            file_name = part
+    print(new_path)
+    if os.path.isdir(new_path):
+        break
+    else:
+        print('такой папки нет в системе ')
+
+if os.path.exists(os.path.join(new_path, file_name)):
+    print('файл будет перезаписан')
+
+with open(os.path.join(new_path, file_name), 'w', encoding='UTF-8') as file:
+    file.write(text)
