dbe491a297a850a57bf04c081e60a6dab5dd6c04
haart <haart@yandex.ru>
2021-04-15 15:11:16 +0300

    task01 done

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+with open('log.txt', 'w', encoding='UTF-8') as log:
+    pass
+
+with open('people.txt', 'r', ) as pfile:
+    summ = 0
+    for line in pfile:
+        try:
+            length = len(line)
+            if line.endswith('\n'):
+                length -= 1
+            if length > 2:
+                summ += length
+            else:
+                raise TypeError('Длина имени меньше 3 символов')
+        except TypeError:
+            # print('ошибка в имени')
+            with open('log.txt', 'a', encoding='UTF-8') as logfile:
+                logfile.write('ошибка длины в имени ' + line )
+
+print('Итоговая сумма', summ)
