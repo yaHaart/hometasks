21243ca46bb37904912788247cc39e6c8628b6cb
haart <haart@yandex.ru>
2021-04-15 20:21:26 +0300

    task05 done

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+summ = 0
+with open('calc.txt', 'r') as file:
+    for line in file:
+        temp_list = line.split()
+
+        try:
+            if len(temp_list) != 3:
+                raise ValueError('неполная строка')
+            if temp_list[1] not in '+-*%//':
+                raise TypeError('неизвестный знак мат действия')
+            if not temp_list[0].isdigit() or not temp_list[2].isdigit():
+                raise ValueError('в строке есть буквы, а не цифры')
+
+            if temp_list[1] == '+':
+                summ += int(temp_list[0]) + int(temp_list[2])
+            elif temp_list[1] == '-':
+                summ += int(temp_list[0]) - int(temp_list[2])
+            elif temp_list[1] == '*':
+                summ += int(temp_list[0]) * int(temp_list[2])
+            elif temp_list[1] == '//':
+                summ += int(temp_list[0]) // int(temp_list[2])
+            elif temp_list[1] == '%':
+                summ += int(temp_list[0]) % int(temp_list[2])
+            elif temp_list[1] == '/':
+                summ += int(temp_list[0]) / int(temp_list[2])
+            else:
+                raise TypeError('Что-то пошло не так. Скорее всего я не знаю такой знак')
+
+        except ValueError as ve:
+            print(ve)
+        except TypeError as te:
+            print(te)
+        except ZeroDivisionError:
+            print('НА 0 делить нельзя')
+
+print()
+print('Финальная сумма всего ', summ)
