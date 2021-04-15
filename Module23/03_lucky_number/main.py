3d3fe944315641bfd199de283c41477258d37f86
haart <haart@yandex.ru>
2021-04-15 16:38:53 +0300

    task03 done

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+from random import randint
+
+with open('numbers.txt', 'w', encoding='UTF-8') as file:
+    pass
+summ = 0
+while summ < 777:
+    try:
+        number = int(input('Введите целое число '))
+        with open('numbers.txt', 'a', encoding='UTF-8') as file:
+            file.write(str(number) + '\n')
+        summ += number
+        roulette = randint(1, 13)
+        print(roulette)
+        if roulette == 13:
+            raise BaseException()
+
+    except ValueError:
+        print('ввведите только число!')
+    except BaseException:
+        print('тебе не повезло')
+        break
