cae129fea5131d2a9dd77494e4aaf80ed91bdd6f
haart <haart@yandex.ru>
2021-04-20 15:09:35 +0300

    task02 done

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -13,27 +13,22 @@ def f2(x, y):
     return y / x
 
 
+with open('result.txt', 'w') as file:
+    pass
+
 try:
-    file = open('coordinates.txt', 'r')
-    for line in file:
-        nums_list = line.split()
-        res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
+    with open('coordinates.txt', 'r') as file:
+        for line in file:
+            nums_list = line.split()
+            # print(nums_list)
+            res1 = f(int(nums_list[0]), int(nums_list[1]))
             res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
+            number = random.randint(0, 100)
+            with open('result.txt', 'a') as file_2:
                 my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
+                file_2.write(' '.join(str(my_list)) + '\n')
+
 except Exception:
-    print("Что-то пошло не так с первой функцией")
-
-
-# TODO отредактировать и исправить программу
+    print("Что-то пошло не так")
+except ZeroDivisionError:
+    print('Случайно поделили на 0')
