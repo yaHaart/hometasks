be6d3ada45afddc8da925ca8f6a90ac07730b690
haart <haart@yandex.ru>
2021-04-28 12:51:34 +0300

    task02 done

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -10,26 +10,55 @@ class Property:
 class Appartment(Property):
     def tax(self):
         print(f'Налог на квартиру стоимостью {self.worth} составляет {self.worth/1000}')
+        return self.worth / 1000
 
 
 class Car(Property):
     def tax(self):
         print(f'Налог на машину стоимостью {self.worth} составляет {self.worth/200}')
+        return self.worth/200
 
 
 class CountryHouse(Property):
     def tax(self):
-        print(f'Налог на дом стоимостью {self.worth} составляет {self.worth/200}')
+        print(f'Налог на дом стоимостью {self.worth} составляет {self.worth/500}')
+        return self.worth/500
 
 
-appart = Appartment(1000000)
-appart.tax()
+text_input = ''
+summ = 0
+while True:
+    text_input = input('какое имущество у вас есть? введите в формате "что сколько стоит" ')
+    if text_input == 'это все':
+        break
+    temp_list = text_input.split()
+    try:
+        if not temp_list[1].isdigit() and not int(temp_list[1]) > 0:
+            raise AttributeError
 
-car = Car(500000)
-car.tax()
+        if temp_list[0].lower() == 'квартира':
+            temp_property = Appartment(int(temp_list[1]))
+            summ += temp_property.tax()
+        elif temp_list[0].lower() == 'машина':
+            temp_property = Car(int(temp_list[1]))
+            summ += temp_property.tax()
+        elif temp_list[0].lower() == 'дом':
+            temp_property = CountryHouse(int(temp_list[1]))
+            summ += temp_property.tax()
+        else:
+            print(' Неверный формат или неизвестная собственность. Только квартира машина или дом')
 
-house = CountryHouse(10000000)
-house.tax()
+    except AttributeError:
+        print('Проблема со стоимостью объекта')
+    except IndexError:
+        print('Проблема в формате. нужно примерно так: квартира 1000')
+    except ValueError:
+        print('стоимость должна быть целым натуральным числом')
 
-appart2 = Appartment()
-appart2.tax()
+
+money = int(input('Сколько денег у вас есть на налоги? '))
+
+if money >= summ:
+    print('У вас хватит денег на оплату налогов')
+else:
+    print(f'У вас не хватает {summ - money} рублей')
