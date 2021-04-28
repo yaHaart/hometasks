af4a4e6fbcafb9b0f5d55a249667d28588203bf1
haart <haart@yandex.ru>
2021-04-28 12:16:33 +0300

    task01 done

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+class Property:
+
+    def __init__(self, worth=0):
+        self.worth = worth
+
+    def tax(self):
+        pass
+
+
+class Appartment(Property):
+    def tax(self):
+        print(f'Налог на квартиру стоимостью {self.worth} составляет {self.worth/1000}')
+
+
+class Car(Property):
+    def tax(self):
+        print(f'Налог на машину стоимостью {self.worth} составляет {self.worth/200}')
+
+
+class CountryHouse(Property):
+    def tax(self):
+        print(f'Налог на дом стоимостью {self.worth} составляет {self.worth/200}')
+
+
+appart = Appartment(1000000)
+appart.tax()
+
+car = Car(500000)
+car.tax()
+
+house = CountryHouse(10000000)
+house.tax()
+
+appart2 = Appartment()
+appart2.tax()
--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def setdefault(self, __key: _KT, __default: _VT = ...) -> _VT:
+
+dict1 = MyDict()
+dict2 = dict()
+
+dict1[1] = 'мой словарь'
+dict2[1] = 'обычный словарь'
+
+print(dict2.get(1))
+print(dict1.get(1))
+print(dict2.get(2))
+print(dict1.get(2))
+print(dict1, dict2)
+dict2.get()
