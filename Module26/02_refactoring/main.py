ae2ba51837fdd754524df118cc6335f82c101b7e
haart <haart@yandex.ru>
2021-05-05 13:08:02 +0300

    task02 done

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -3,15 +3,10 @@ list_2 = [3, 8, 4, 9]
 to_find = 56
 
 can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
+res = (x * y for x in list_1 for y in list_2)
+for i in res:
+    print(i)
+    if i == to_find:
+        print('Found!!!')
+        can_continue = False
         break
-
-# TODO провести рефакторинг кода
