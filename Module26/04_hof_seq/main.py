cac3b780e4829fcd848670696fa50a5a96c797e9
haart <haart@yandex.ru>
2021-05-06 16:42:09 +0300

    task04 done

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def q_sequence(n):
+    q_list = []
+    for j in range(n):
+        if j < 2:
+            q_list.append(1)
+            yield 1
+        else:
+            q_list.append(q_list[j - q_list[j - 1]] + q_list[j - q_list[j - 2]])
+            yield q_list[j - q_list[j - 1]] + q_list[j - q_list[j - 2]]
+
+
+q = q_sequence(20)
+for i in q:
+    print(i, end=' ')
